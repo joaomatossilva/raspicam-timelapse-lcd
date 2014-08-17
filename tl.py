d30e8940631dffad23764671eefe327bb6c5e0ef
Joao Matos Silva <kappy@acydburne.com.pt>
2014-08-17 17:33:35 +0100

    added cicle and improved sleep timer

--- tl.py
+++ tl.py
@@ -4,17 +4,12 @@ import time
 import datetime
 import os
 import picamera
-#from Adafruit_I2C import Adafruit_I2C
-#from Adafruit_MCP230xx import Adafruit_MCP230XX
 import Adafruit_CharLCD as LCD
 
-#VIDEO_DAYS = 5
 FRAMES_PER_HOUR = 6
-#FRAMES = FRAMES_PER_HOUR * 24 * VIDEO_DAYS
 ROTATION = 90
 
 lcd = LCD.Adafruit_CharLCDPlate()
-#lcd.backlight(lcd.ON)
 
 def capture_frame(path, frame):
     lcd.clear()
@@ -24,27 +19,38 @@ def capture_frame(path, frame):
         time.sleep(2)
         cam.capture('%s/frame%03d.jpg' % (path, frame))
 
-#create directory with date
-date = datetime.datetime.now().strftime('%b %d')
-directory = '/home/pi/%s' % date
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def time_lapse()
+    #create directory with date
+    date = datetime.datetime.now().strftime('%Y%m%d')
+    directory = '/home/pi/%s' % date
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-frame = 0
-# Capture the images
-while True:
-    # Note the time before the capture
-    frame = frame + 1
-    start = time.time()
-    capture_frame(directory, frame)
-    # Wait for the next capture. Note that we take into
-    # account the length of time it took to capture the
-    # image when calculating the delay
+    frame = 0
+    # Capture the images
     while True:
-        seconds_to_next = int(60 * 60 / FRAMES_PER_HOUR) - (time.time() - start)
-        if seconds_to_next <= 0:
-            break
-        lcd.clear()
-        lcd.message('taken: %03d\n' % frame)
-        lcd.message('next: %s' % str(datetime.timedelta(seconds=seconds_to_next)))
-        time.sleep(1)
+        # Note the time before the capture
+        frame = frame + 1
+        start = time.time()
+        capture_frame(directory, frame)
+        # Wait for the next capture. Note that we take into
+        # account the length of time it took to capture the
+        # image when calculating the delay
+        while True:
+            seconds_to_next = int(60 * 60 / FRAMES_PER_HOUR) - (time.time() - start)
+            if seconds_to_next <= 0:
+                break
+            lcd.clear()
+            lcd.message('taken: %03d\n' % frame)
+            lcd.message('next: %s' % str(datetime.timedelta(seconds=seconds_to_next)))
+            if lcd.is_pressed(LCD.SELECT):
+                return
+            time.sleep(5 - (time.time() - start))
+
+while True:
+    lc.clear()
+    lcd.message('Press SELECT to\n start...')
+    if not lcd.is_pressed(LCD.SELECT):
+        continue
+    time_lapse()
+
